d649d303811952bd32dfd2fbdd5e31f35674180b
Wil T <wil.t.me@pm.me>
2025-03-15 15:02:35 -0400

    better use of pathlib in test_build_text.py

--- tests/main/test_build_text.py
+++ tests/main/test_build_text.py
@@ -5,7 +5,6 @@ from src.build_processing import get_readme
 
 def test_readme_html() -> None:
     """Ensure that markdown text is being converted to html."""
-    with Path.open(Path(__file__).resolve().parent / "markdown_example/markdown.html", encoding="utf-8") as file:
-        html = file.read()
-    zip_path = Path(__file__).resolve().parent / "markdown_example/markdown.ext"
+    html = (Path(__file__).parent / "markdown_example/markdown.html").read_text(encoding="utf-8")
+    zip_path = Path(__file__).parent / "markdown_example/markdown.ext"
     assert f"{get_readme([zip_path])}\n" == html
