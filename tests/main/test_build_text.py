b7bb62cee710bfe9c56dca7bcc7570076a737060
Wil T <wil.t.me@pm.me>
2025-03-15 14:50:53 -0400

    move markdown.zip to markdown.ext

--- tests/main/test_build_text.py
+++ tests/main/test_build_text.py
@@ -7,5 +7,5 @@ def test_readme_html() -> None:
     """Ensure that markdown text is being converted to html."""
     with Path.open(Path(__file__).resolve().parent / "markdown_example/markdown.html", encoding="utf-8") as file:
         html = file.read()
-    zip_path = Path(__file__).resolve().parent / "markdown_example/markdown.zip"
+    zip_path = Path(__file__).resolve().parent / "markdown_example/markdown.ext"
     assert f"{get_readme([zip_path])}\n" == html
