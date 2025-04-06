03b0945188d62d0b45e4a0dd5aa561307d6fa62c
Wil T <wil.t.me@pm.me>
2025-04-06 12:32:16 -0400

    fix user agent

--- tests/main/test_configure_headless_chrome.py
+++ tests/main/test_configure_headless_chrome.py
@@ -1,3 +1,5 @@
+import importlib.metadata
+
 from src.main import configure_headless_chrome
 
 
@@ -10,9 +12,12 @@ def test_configure_headless_chrome() -> None:
     args = convert_args_to_dict(options.arguments)
     assert "--headless" in args  # headless mode is active
     assert args["--headless"] == "new"  # headless mode is using new mode
+
     assert "--window-size" in args
     assert int(args["--window-size"][0]) > 1024  # window size is at least 1024 wide
     assert int(args["--window-size"][1]) > 800  # window size is at least 800 tall
+
     assert "--user-agent" in args
+    assert args["--user-agent"] == f"Mozilla/5.0 (compatible; FG-Forge-Updater/{importlib.metadata.version("fg-forge-updater")}; +https://github.com/bmos/FG-Forge-Updater)"
     assert args["--user-agent"].__contains__("Mozilla/5.0 (compatible; FG-Forge-Updater")
     assert args["--user-agent"].__contains__("https://github.com/bmos/FG-Forge-Updater")
